95509d8a658c9b3a07528a0e6bc0c9cf45f68af2
Gan356RSM <151418866+Gan356RSM@users.noreply.github.com>
2025-04-27 11:09:04 +0700

    Update conicalFlask.py

--- LETMEKNOW/conicalFlask.py
+++ LETMEKNOW/conicalFlask.py
@@ -1,16 +1,29 @@
-from flask import Flask, render_template, request, redirect, session, flash
+from flask import Flask, render_template, request, redirect, session, flash, g
 import os
+import sqlite3
 
-users = [ {"uid": 0, "name": "Mahito", "password": "Idle_Transfigure" }]
+def open_db():
+    if not 'db' in g:
+        g.db = sqlite3.connect("blog.db")
+    return g.db
+
+'''
+users = [ {"uid": 0, "name": "tim", "password": "password1" }, 
+          {"uid": 1, "name": "jim", "password": "password2" }]
+'''
 
 blog_posts = [
               {"pid": 0,
-               "title": "Nanami",
-               "content": "I can't allow this fight to go overtime."
+               "title": "A Post About Cats",
+               "content": "Cats are cool.  I like cats"
               },
               {"pid": 1,
-               "title": "Mahito",
-               "content": "I'm- I'M TRULY- A CURSE!!! BLACK FLASH."
+               "title": "A Post About Dogs",
+               "content": "Dogs are cool.  I like dogs"
+              },
+              {"pid": 2,
+               "title": "A Post About Birds",
+               "content": "Birds are cool.  I like birds"
               }
              ]
 
@@ -35,6 +48,10 @@ def show_posts():
 
 def create_post():
     if request.method == "POST":
+        if session.get('uid') == None:
+            flash("Log in to make a post") 
+            return redirect("/login")
+
         title = request.form["title"]
         content = request.form["content"]
         blog_post = { "pid": len(blog_posts), "content": content, "title": title }
@@ -43,42 +60,60 @@ def create_post():
     else:
         return render_template("create_post.html")
 
+#sign page
 def account():
+    if request.method == "POST":
+        name = request.form["name"]
+        password = request.form["password"]
+        db = open_db()
+        db.execute("INSERT INTO accounts (username, password) VALUES('" + name + "', '" + password + "')")
+        db.commit()
+
+        results = db.execute("SELECT username, password FROM accounts").fetchall()
+        print(results)
+
+        return redirect
+
     return render_template("account.html")
 
 def login():
     if request.method == "POST":
         name = request.form["name"]
         password = request.form["password"]
-
-        error = "Error Idioto"
+        
+        error = "Incorrect credentials"
         uid = -1
-        for user in users:
-            if user["name"] == name and user["password"] == password:
+        db = open_db()
+        results = db.execute("SELECT rowid, username, password FROM accounts").fetchall()
+        for row in results:
+            if row[1] == name and row[2] == password:
                 error = None
-                uid = user["uid"]
+                uid = row[0]
+
         if error is None:
             session.clear()
-            session["uid"] = str(uid)
+            session['uid'] = uid
+            flash(error)
             return redirect("/")
         else:
             flash(error)
             return render_template("login.html")
+
     else:
         return render_template("login.html")
 
 def logout():
-    session.clear()
+    session.clear() 
     return redirect("/")
 
 app = Flask(__name__, template_folder=os.getcwd(), static_folder=os.getcwd())
-app.config.from_mapping(SECRET_KEY="my_dev_key")
+app.config.from_mapping(SECRET_KEY='my_dev_key')
 
 app.add_url_rule("/", "index", index)
 app.add_url_rule("/show_posts", "show_posts", show_posts)
 app.add_url_rule("/create_post", "create_post", create_post, methods=["GET", "POST"])
 app.add_url_rule("/account", "account", account)
-app.add_url_rule("/login", "login", login, methods=["GET","POST"])
+app.add_url_rule("/login", "login", login, methods=["GET", "POST"])
 app.add_url_rule("/logout", "logout", logout, methods=["POST"])
-
 app.run()
+
